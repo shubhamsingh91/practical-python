eaa2f3f48b8e45ed81f12729c8f26114eeb92b56
shubhamsingh91 <shubham.singh091@gmail.com>
2025-11-16 22:38:20 -0600

    adding report

--- Work/report.py
+++ Work/report.py
@@ -3,6 +3,7 @@
 # Exercise 2.4
 
 import csv
+from pprint import pprint
 
 def read_portfolio(filename):
     'Opens a portfolio file and returns a list of tuples'
@@ -21,10 +22,58 @@ filename = "Data/portfolio.csv"
 
 portfolio = read_portfolio(filename)
 
-print(portfolio)
 total_cost = 0
 
 for name,shares,price in portfolio:
     total_cost+= price*shares
 
 print("total_cost = ",total_cost)
+
+
+def read_portfolio_dict(filename):
+    'Opens a portfolio file and returns a list of dictionary'
+    portfolio = []
+    with open(filename,'rt') as file:
+        next(file) # skipping the header
+
+        rows = csv.reader(file)
+        for row in rows:
+            portfolio.append({'name': row[0], 'price': float(row[2]), 'shares': int(row[1])})
+        
+    return portfolio
+
+portfolio_dict = read_portfolio_dict(filename)
+
+total_cost = 0
+
+for d in portfolio_dict:
+    total_cost+= d['price']*d['shares']
+
+print("total_cost = ",total_cost)
+# pprint(portfolio_dict)
+
+filename = "Data/prices.csv"
+
+def read_prices(filename):
+    file = open(filename,'rt')
+    rows = csv.reader(file)
+    prices = {}
+
+    for row in rows:
+        if (len(row)!=0):
+            prices[row[0]] = float(row[1])
+    
+    return prices
+
+prices = read_prices(filename)
+
+# computing total value of the portfolio
+current_value = 0
+
+for p in portfolio_dict:
+    current_value += p['shares']*prices[p['name']]
+
+print("current_value = ", current_value)
+print("loss = ",  total_cost-current_value)
+
+
